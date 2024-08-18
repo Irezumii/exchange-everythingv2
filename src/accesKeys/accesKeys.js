562b8b5dcda54a16e486cfb8958a6f6cf0242a8e
Konrad Sordyl <konio180@gmail.com>
2024-08-18 23:01:17 +0200

    u

--- src/accesKeys/accesKeys.js
+++ src/accesKeys/accesKeys.js
@@ -1,6 +1,6 @@
 //100 fetches each per month
-export const coinLayerKeys = "7adaf56f2b86b6541e27389da0c76a7e"
+export const coinLayerKeys = ["7adaf56f2b86b6541e27389da0c76a7e"]
 
 //500 fetches each per month
-export const eodhdKeys = "66c2573d73d8e8.28482182"
+export const eodhdKeys = ["66c2573d73d8e8.28482182"]
 
